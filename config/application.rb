03241aa5a05fc06d490593eec62b431dfc7bcd17
Derek Young <derekpyoung12@gmail.com>
2023-03-22 15:59:39 -0700

    removed config

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "action_cable/engine"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 module PowHounds
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
